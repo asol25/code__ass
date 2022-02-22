4d0a0ccfb6d1915cca7862e778d094df86c2372b
asol25 <usool.203@gmail.com>
2022-02-22 10:14:40 +0700

    add

--- css/gameupdate/app.js
+++ css/gameupdate/app.js
@@ -1,7 +1,7 @@
 const button = document.querySelectorAll("button.btn");
 const main = document.getElementById("section--content");
 
-const APIURL = '../css/gameupdate/data.json';
+const APIURL = '../json/update/data.json';
 
 // async function loadAsset() {
 //     const resp = await fetch(APIURL);
